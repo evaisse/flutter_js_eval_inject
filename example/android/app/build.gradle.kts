c03e81dd068b7a080d33301e83a72cc7a49fd194
Emmanuel VAÏSSE <evaisse@gmail.com>
2025-09-18 18:47:58 +0200

    feat: demo app

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_js_eval_inject_example"
+    namespace = "com.evaisse.flutter_js_eval_inject_example"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_js_eval_inject_example"
+        applicationId = "com.evaisse.flutter_js_eval_inject_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
