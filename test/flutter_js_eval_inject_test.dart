82050bd278350c9cc070d5e6c5b2fbaafca118fa
Emmanuel VAÏSSE <evaisse@gmail.com>
2025-09-18 18:54:40 +0200

    test: update plugin test to verify API exports

--- test/flutter_js_eval_inject_test.dart
+++ test/flutter_js_eval_inject_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_js_eval_inject/flutter_js_eval_inject.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Plugin exports are accessible', () {
+    expect(FlutterJsEvalInject.addJavascriptFile, isNotNull);
+    expect(FlutterJsEvalInject.addJavascriptCode, isNotNull);
+    expect(FlutterJsEvalInject.evalJavascript, isNotNull);
   });
 }
